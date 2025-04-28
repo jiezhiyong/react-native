afd04c7916751e51fdca2637b9b72b75c76d9309
jzy42459 <jzy42459@ly.com>
2025-04-28 15:57:55 +0800

    chore: update

--- app/discover/manifests.tsx
+++
@@ -1,3 +0,0 @@
-export default function ExpoManifestsScreen() {
-  return <ScrollView className="flex-1 p-6"></ScrollView>;
-}
--- app/discover/skia.tsx
+++
@@ -1,3 +0,0 @@
-export default function SkiaScreen() {
-  return <ScrollView className="flex-1 p-6"></ScrollView>;
-}
