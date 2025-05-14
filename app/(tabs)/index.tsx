564de1804db983e1b4c79ec0947a1224a62f1fc7
jzy42459 <jzy42459@ly.com>
2025-05-14 12:01:44 +0800

    chore: update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,23 @@
-import { useRouter } from 'expo-router';
+import { MasonryFlashList } from '@shopify/flash-list';
+import { Image } from 'expo-image';
 import { setStatusBarStyle } from 'expo-status-bar';
 import * as React from 'react';
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { ActivityIndicator, Animated, Image, RefreshControl, View } from 'react-native';
-import { ResponsiveGrid } from 'react-native-flexible-grid';
+import { ActivityIndicator, Animated, RefreshControl, View } from 'react-native';
 
 import { BREAK_POINT } from '~/components/ui/custom-header';
+import { Skeleton } from '~/components/ui/skeleton';
+import { Text } from '~/components/ui/text';
 import { sleep } from '~/lib/utils';
 import { useTabsScrollStore } from '~/store/scroll';
 
-interface DataProp {
+interface DataItem {
   id: number;
-  widthRatio?: number;
-  heightRatio?: number;
-  imageUrl?: string;
+  skeletonNum: number;
 }
 
 export default function HomeScreen() {
-  const [data, setData] = useState<DataProp[]>([]);
+  const [data, setData] = useState<DataItem[]>([]);
   const [refreshing, setRefreshing] = useState(false);
   const [isLoadingMore, setIsLoadingMore] = useState(false);
 
@@ -29,7 +29,7 @@ export default function HomeScreen() {
   const handleRefresh = useCallback(async () => {
     setRefreshing(true);
     await sleep(1500);
-    const initData = getData(1);
+    const initData = getData();
     setData(initData);
     setRefreshing(false);
   }, []);
@@ -57,24 +57,23 @@ export default function HomeScreen() {
   );
 
   // 获取 Mock 数据
-  const getData = (repeatedTimes: number) => {
+  const getData = () => {
     const originalData = [
-      { widthRatio: 1, heightRatio: 4 },
-      { widthRatio: 1, heightRatio: 3 },
-      { widthRatio: 1, heightRatio: 4 },
-      { widthRatio: 1, heightRatio: 5 },
-      { widthRatio: 1, heightRatio: 5 },
-      { widthRatio: 1, heightRatio: 3 },
+      { skeletonNum: 1 },
+      { skeletonNum: 3 },
+      { skeletonNum: 3 },
+      { skeletonNum: 1 },
+      { skeletonNum: 2 },
+      { skeletonNum: 3 },
     ];
 
-    let clonedData: DataProp[] = [];
-    for (let i = 0; i < repeatedTimes; i++) {
-      const newData = originalData.map((item) => ({
-        ...item,
-        id: ++idCounter.current,
-      }));
-      clonedData = [...clonedData, ...newData];
-    }
+    let clonedData: DataItem[] = [];
+    const newData = originalData.map((item) => ({
+      ...item,
+      id: ++idCounter.current,
+      skeletonNum: item.skeletonNum,
+    }));
+    clonedData = [...clonedData, ...newData];
 
     return clonedData;
   };
@@ -88,7 +87,7 @@ export default function HomeScreen() {
     setIsLoadingMore(true);
 
     await sleep(1500);
-    const newData = getData(1);
+    const newData = getData();
     setData((prevData) => [...prevData, ...newData]);
     setIsLoadingMore(false);
   };
@@ -102,35 +101,53 @@ export default function HomeScreen() {
     );
   };
 
-  const renderItem = ({ item }: { item: DataProp }) => {
+  const renderItem = ({ item }: { item: DataItem }) => {
     return (
-      <View className="flex-1 w-full px-1 pt-2 bg-red-100">
-        <Image
-          source={require('~/assets/images/home-header-bg.jpg')}
-          className="w-full h-full rounded-xl border border-border p-1"
-          resizeMode="cover"
-        />
+      <View className="px-1 pt-2">
+        <View className="flex-1 w-full overflow-hidden rounded-xl border border-border p-3">
+          <View className="w-full aspect-square">
+            <Image
+              source={require('~/assets/images/home-header-bg.jpg')}
+              className="w-full h-auto"
+              style={{ aspectRatio: 1, borderRadius: 6 }}
+              contentFit="cover"
+            />
+          </View>
+
+          {Array.from({ length: item.skeletonNum }).map((_, index) => (
+            <View key={index}>
+              <Skeleton className="w-full h-5 rounded-md mt-2" />
+              <Skeleton className="w-3/5 h-5 rounded-md mt-2" />
+            </View>
+          ))}
+        </View>
       </View>
     );
   };
 
   return (
-    <ResponsiveGrid
-      keyExtractor={(item: DataProp) => item.id.toString()}
+    <MasonryFlashList
+      keyExtractor={(item) => item.id.toString()}
       onScroll={handleScroll}
-      scrollEventInterval={16}
-      maxItemsPerColumn={2}
-      data={data}
-      renderItem={renderItem}
-      itemUnitHeight={80}
-      HeaderComponent={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
-      FooterComponent={renderFooter}
+      refreshing={refreshing}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
       onEndReached={loadData}
-      onEndReachedThreshold={0.2}
-      style={{
-        paddingTop: 16,
-        paddingHorizontal: 16,
-      }}
+      data={data}
+      numColumns={2}
+      renderItem={renderItem}
+      ListFooterComponent={renderFooter}
+      ListHeaderComponent={
+        <View className="w-full rounded-xl overflow-hidden pt-5 pb-1 px-1">
+          <Image
+            source={require('~/assets/images/home-header-bg.jpg')}
+            style={{ height: 120, borderRadius: 6 }}
+            contentFit="cover"
+          />
+        </View>
+      }
+      ListEmptyComponent={null}
+      contentContainerStyle={{ paddingHorizontal: 16 }}
+      optimizeItemArrangement={false}
     />
   );
 }
---
+++ components/Empty.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+
+import { Text } from './ui/text';
+
+export default function Empty() {
+  return (
+    <View>
+      <Text>Empty</Text>
+    </View>
+  );
+}
