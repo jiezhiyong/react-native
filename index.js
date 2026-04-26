0489b7310eecfbe3e2b60b187bc9c13b1515cf95
jiezhiyong <9351855+jiezhiyong@users.noreply.github.com>
2026-04-26 17:29:21 +0800

    feat: add FormatJS polyfills for improved localization support
    - Introduced FormatJS polyfills for Intl capabilities to prevent runtime errors in iOS simulator when using typesafe-i18n.
    - Added necessary locale data for plural rules in English and Chinese to enhance internationalization features.

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 import { LogBox } from 'react-native';
 
 /**
+ * FormatJS polyfills
+ * typesafe-i18n 渲染时会调用 new Intl.PluralRules(locale)，iOS simulator 的运行时缺少对应 Intl 能力时就会在 <TypesafeI18n> 处崩掉
+ */
+import '@formatjs/intl-getcanonicallocales/polyfill';
+import '@formatjs/intl-locale/polyfill';
+import '@formatjs/intl-pluralrules/polyfill';
+import '@formatjs/intl-pluralrules/locale-data/en';
+import '@formatjs/intl-pluralrules/locale-data/zh';
+
+/**
  * 主要用于 React Native LogBox UI
  * 不适合屏蔽 React Native Web 直接输出到浏览器 console 的 warning。
  */
